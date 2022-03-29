3c2b0a96e89ac638320707d4087967972fe8bf78
Axtol <102456067+Axtol@users.noreply.github.com>
2022-03-29 22:47:41 +0800

    Add files via upload

---
+++ V1.3.2_All source code/ide.py
@@ -0,0 +1,29 @@
+def c():
+    while True:
+        cmd = input('>>> ')
+        if cmd == "quit" or cmd == "quit()" or cmd == "exit" or cmd == "exit()":
+            raise OSError("程序结束")
+        last = cmd[-1]
+        while last == ':' or last == '\\':
+            temp = input('... ')
+            cmd += temp
+            last = cmd[-1]
+        try:
+            res = eval(cmd)
+            print(res)
+        except BaseException as e:
+            try:
+                exec(cmd)
+            except BaseException as e:
+                print(e)
+
+
+def e():
+    try:
+        c()
+    except IndexError:
+        e()
+
+
+if __name__ == "__main__":
+    e()
